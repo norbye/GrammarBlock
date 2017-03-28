9c2e942c8b953e150c3435e455f2039115f36645
Jonatan Norbye <jonatan@norbye.com>
2017-03-28 20:46:53 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Checking page title
+
+/*
+ * Message wrapper: div._1t_p
+ * Name tag: h5._ih3 > span._3oh- > span
+ *          div.clearfix _o46 _3erg _3i_m _nd_ direction_ltr text_align_ltr
+ *          div.clearfix _o46 _3erg _29_7 direction_ltr text_align_ltr 
+ * Message: div.clearfix _o46 _3erg
+      div._3058 _ui9 _hh7 _aol _s1- _52mr _43by _3oh-
+      div._3058 _ui9 _hh7 _aol _s1- _52mr _3oh- 
+    > div._3058 _ui9 _hh7 _aol _s1- _52mr _3oh- 
+        > div._aok 
+            > span._3oh- _58nk 
+                > span
+*/
+
+if (document.title.indexOf("Messenger") != -1) {
+    //Creating Elements
+    
+}
